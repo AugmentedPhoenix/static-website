d67addb1f94cab77e66fd79e5f0dc3910f14b053
Bjorn <bjornvanwilligen@hotmai.com>
2022-04-06 10:55:38 +0200

    added Hesperos II

--- src/components/Card.js
+++ src/components/Card.js
@@ -78,6 +78,9 @@ const Card = ({inGameName1, server1, inGameName2, server2, isFlipped, isInfoFlip
                         <li className={`card-li`}>
                             <p className={`card-li-p`}>Hesperos</p>
                         </li>
+                        <li className={`card-li`}>
+                            <p className={`card-li-p`}>Hesperos II</p>
+                        </li>
                     </ul>
                 </div>
             </div>
